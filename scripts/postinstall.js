33032c155b871ca3a8e473d952ba878763cc94f3
aGamingGod1234 <agaminggod12345@gmail.com>
2026-02-22 00:40:09 +0800

    sync scripts/postinstall.js

---
+++ scripts/postinstall.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const { ensureInstalled } = require('./install-lib');
+
+try {
+  const details = ensureInstalled({ setStartupEntry: true });
+  console.log(`NetQualityTray installed to ${details.installDir}`);
+} catch (error) {
+  console.error(`NetQualityTray postinstall failed: ${error.message}`);
+  process.exit(1);
+}
